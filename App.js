ab9dfb275ea6f48e8679c0ee511c6bbc2f3298f1
Sharmaine <sharmaine.borbe@students.dominican.edu>
2024-04-10 14:54:57 -0700

    completed challenges 1-7

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, FlatList, SafeAreaView, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
-
+import { StyleSheet, View, FlatList, SafeAreaView, TextInput, KeyboardAvoidingView, Platform, Button } from 'react-native';
 import Item from './Item';
-import { cats } from './breeds';
+import { cats, dogs } from './breeds';
 
 export default function App() {
-  const [search, setSearch] = useState('')
+  const [search, setSearch] = useState('');
+  const [showCats, setShowCats] = useState(true);
+
+  const dataToShow = showCats ? cats : dogs;
 
   return (
     <SafeAreaView style={styles.container}>
@@ -14,26 +16,31 @@ export default function App() {
         behavior={Platform.OS === "ios" ? "padding" : "height"}
         style={styles.kav}
       >
-      <StatusBar style="auto" />
-      <View style={styles.listContainer}>
-        <FlatList 
-          data={cats.filter(item => item.breed.includes(search))}
-          renderItem={({ item, index }) => {
-            return <Item index={index} data={item} />
-          }}
-          keyExtractor={item => item.breed}
-        />  
-      </View>
-      <View>
-      <TextInput 
-        style={styles.search}
-        placeholder='Search'
-        onChangeText={setSearch}
-        value={search}
-      />
-      </View>
+        <StatusBar style="auto" />
+        <View style={styles.listContainer}>
+          <FlatList 
+            data={dataToShow.filter(item => item.breed.includes(search))}
+            renderItem={({ item, index }) => {
+              return <Item index={index} data={item} />
+            }}
+            keyExtractor={item => item.breed}
+          />  
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput 
+            style={styles.search}
+            placeholder='Search'
+            onChangeText={setSearch}
+            value={search}
+          />
+          <Button
+            title={showCats ? 'Show Dogs' : 'Show Cats'}
+            onPress={() => setShowCats(!showCats)}
+            color="#841584"
+          />
+        </View>
       </KeyboardAvoidingView>
-      </SafeAreaView>
+    </SafeAreaView>
   );
 }
 
@@ -45,7 +52,8 @@ const styles = StyleSheet.create({
     marginBottom: 30,
   },
   listContainer: {
-    width: '100%'
+    flex: 1,
+    width: '100%',
   },
   container: {
     flex: 1,
@@ -54,8 +62,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   search: {
+    flex: 1,
     fontSize: 24,
     padding: 10,
     borderWidth: 1,
-  }
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
 });
